1ed37a48efbc89753cbdb03b762ac713517b823d
Brett L Kleinschmidt <brett@spacecurve.com>
2012-11-27 12:17:06 -0800

    update

--- fanctl.sh
+++ fanctl.sh
@@ -1,52 +1,54 @@
-#! /bin/sh
+#!/bin/bash
 
-#
-# Controls the fan speed for MacBook Pro
-# It's best to background this script from rc.local
-#
+# path to applesmc (in sysfs)
+asmc="/sys/devices/platform/applesmc.768"
 
-applesmc_path="/sys/devices/platform/applesmc.768"
+# fan to control
+fid="fan1"
+
+# thermo to read
+tid="temp7"
+
+# time between runs
 interval=5
 
-# enable manual fan setting
-echo 1 >${applesmc_path}/fan1_manual
-
-function set_fans()
-{
-    echo "$1" >${applesmc_path}/fan1_output
+# frequency scaling model
+function freq_model {
+    echo $(($1 * 200 - 10000))
 }
 
-function get_temp()
-{
-    local temp_no=7 # thermo number, closest to CPU
-    local temp=$(cat ${applesmc_path}/temp${temp_no}_input)
-    echo $((temp/1000))
+# don't edit below this line
+function normalize {
+    [[ $1 -lt $2 ]] && echo $2 && return
+    [[ $1 -gt $3 ]] && echo $3 && return
+    echo $1
 }
 
-function rpm_from_temp()
-{
-    local speed_min=2000 #$(cat ${applesmc_path}/fan1_min)
-    local speed_max=6500 #$(cat ${applesmc_path}/fan1_max)
-    local speed=$(($((200 * $1)) - 10000))
-    if [[ $speed -lt $speed_min ]]; then
-        speed=$speed_min
-    elif [[ $speed -gt $speed_max ]]; then
-        speed=$speed_max
-    fi
-    echo $speed
+# control paths
+_fan=${asmc}/${fid}
+_temp=${asmc}/${tid}
+
+read _fan_min < ${_fan}_min
+read _fan_max < ${_fan}_max
+
+function run {
+    read temp
+    temp=$((${temp} / 1000))
+    freq=$(freq_model ${temp})
+    freq=$(normalize ${freq} ${_fan_min} ${_fan_max})
+    smsg=$(printf "%s | `date` | %2i C (%s) | %4i rpm (%s)\n" ${0} ${temp} ${tid} ${freq} ${fid})
+    echo ${freq}
 }
 
-TT=$(get_temp)
-ff=$(rpm_from_temp $TT)
-while [ 1 ]
-do
-    T=$(get_temp)
-    f=$(rpm_from_temp $T)
-    set_fans $f
-    printf '%s (%s) status: [ %i (%+i) C | %i (%+i) rpm ]\n' \
-        "$0" "$(date)" $T $(($T-$TT)) $f $(($f-$ff))
-    TT=$T
-    ff=$f
-    sleep $interval &
-    wait $!
+# you must enable manual fan setting
+echo "1" 2>/dev/null > ${_fan}_manual
+if [[ ${?} -ne 0 ]] ; then
+    echo "Run it as root." && exit 1
+fi
+
+while [ 1 ] ; do
+    run < ${_temp}_input > ${_fan}_output
+    echo ${smsg} && sleep ${interval} & wait ${!}
 done
+
+
